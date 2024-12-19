1b6875c71f7ac6e825b2b15f939ed0c7c4d9ab07
TonmoyB <tonmoybtonoy2@gmail.com>
2024-12-19 13:25:21 +0600

    sort implemented

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -11,8 +11,23 @@ import { CartService } from 'src/app/services/cart/cart.service';
 export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
+  originalProducts: Product[] = [];
 
   constructor(private router: Router, private cartService: CartService) { }
+  ngOnInit(): void {
+    this.checkLink();
+  }
+
+  checkLink(): void {
+    if (this.router.url === '/men') {
+      this.originalProducts = this.menProducts;
+    } else if (this.router.url === '/women') {
+      this.originalProducts = this.womenProducts;
+    } else {
+      this.originalProducts = this.allProducts;
+    }
+    this.products = [...this.originalProducts];
+  }
 
   navigateToProduct(product: Product) {
     this.router.navigate(['/product', product.id], {
@@ -30,13 +45,19 @@ export class ProductsComponent implements OnInit {
     alert(`${product.name} has been added to your cart.`);
   }
 
-  ngOnInit(): void {
-    if (this.router.url === '/men') {
-      this.products = this.menProducts;
-    } else if (this.router.url === '/women') {
-      this.products = this.womenProducts;
-    } else {
-      this.products = this.allProducts;
+  onSortChange(event: Event) {
+    const sortBy = (event.target as HTMLSelectElement).value;
+
+    if (sortBy === 'default') {
+      this.products = [...this.originalProducts];
+    } else if (sortBy === 'priceLowHigh') {
+      this.products.sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'priceHighLow') {
+      this.products.sort((a, b) => b.price - a.price);
+    } else if (sortBy === 'nameAZ') {
+      this.products.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortBy === 'nameZA') {
+      this.products.sort((a, b) => b.name.localeCompare(a.name));
     }
   }
 
@@ -45,51 +66,128 @@ export class ProductsComponent implements OnInit {
       id: "1",
       name: 'Souls Diamond',
       price: 185.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Symphony Of Souls Diamond Engagement Ring For Men.png',
+      type: 'ring'
     },
     {
       id: "2",
-      name: 'Elegant Golden Ring',
+      name: 'Contemporary Matte',
       price: 210.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Contemporary Matte Finger Ring.png',
+      type: 'ring'
     },
     {
       id: "3",
-      name: 'Classic Leather Bag',
+      name: 'Divine Pavanputra',
       price: 120.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Divine Pavanputra Pendant.png',
+      type: 'pendant'
     },
     {
       id: '4',
-      name: 'Fashionable Sunglasses',
+      name: 'Dynamic Edge',
       price: 99.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Dynamic Edge Finger Ring.png',
+      type: 'ring'
     },
     {
       id: "5",
-      name: 'Classic Leather Bag',
+      name: 'Elegant Criss Cross',
       price: 120.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Elegant Criss Cross Gold Chain.png',
+      type: 'chain'
     },
     {
       id: '6',
-      name: 'Fashionable Sunglasses',
+      name: 'Elegant Gold Chain',
       price: 99.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Elegant Gold Chain.png',
+      type: 'chain'
     },
     {
       id: "7",
-      name: 'Classic Leather Bag',
+      name: 'Grided Glitz',
       price: 120.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Grided Glitz Gold & Diamond.png',
+      type: 'ring'
     },
     {
       id: '8',
-      name: 'Fashionable Sunglasses',
+      name: 'Lumos Diamond Ring',
       price: 99.00,
-      image: '../../../assets/Symphony Of Souls Diamond Engagement Ring For Men.png'
+      image: '../../../assets/men/Lumos Diamond Ring.png',
+      type: 'ring'
+    },
+    {
+      id: "9",
+      name: `Luxurious Mens' Chain`,
+      price: 185.00,
+      image: `../../../assets/men/Luxurious Men's Gold Chain.png`,
+      type: 'chain'
+    },
+    {
+      id: "10",
+      name: `Maze Mens' Gold Ring`,
+      price: 210.00,
+      image: `../../../assets/men/Maze Men's Gold Finger Ring.png`,
+      type: 'ring'
+    },
+    {
+      id: "11",
+      name: 'Minimalistic Mesmerising',
+      price: 120.00,
+      image: '../../../assets/men/Minimalistic Mesmerising.png',
+      type: 'ring'
+    },
+    {
+      id: '12',
+      name: 'Minimalistic Platimun',
+      price: 99.00,
+      image: '../../../assets/men/Minimalistic Platinum Finger Ring.png',
+      type: 'ring'
+    },
+    {
+      id: "13",
+      name: `Sculpted Mens' Gold Ring`,
+      price: 120.00,
+      image: `../../../assets/men/Sculpted Men's Gold Finger Ring.png`,
+      type: 'ring'
+    },
+    {
+      id: '14',
+      name: 'Sleek Platinum Chain',
+      price: 99.00,
+      image: '../../../assets/men/Sleek Platinum Chain.png',
+      type: 'chain'
+    },
+    {
+      id: "15",
+      name: `Structured Mens'`,
+      price: 120.00,
+      image: '../../../assets/men/Structured Mens Diamond Finger Ring.png',
+      type: 'ring'
+    },
+    {
+      id: '16',
+      name: 'Stunning Geometric',
+      price: 99.00,
+      image: '../../../assets/men/Stunning Geometric Gold Ring for Men.png',
+      type: 'ring'
+    },
+    {
+      id: '17',
+      name: 'Timeless Statement.png',
+      price: 99.00,
+      image: '../../../assets/men/Timeless Statement Gold Chain.png',
+      type: 'chain'
+    },
+    {
+      id: '18',
+      name: 'Rudraksh Bracelet.png',
+      price: 99.00,
+      image: '../../../assets/men/Unique Om Rudraksh Bracelet.png',
+      type: 'bracelet'
     }
-
   ];
 
   womenProducts = [
